53f481a60cc95afe99a9fba0dd572f47a096da41
plockwood <paul.lockwood@education.gov.uk>
2024-12-03 16:39:22 +0000

    add support project

--- src/Dfe.RegionalImprovementForStandardsAndExcellence/Pages/Summary.cshtml.cs
+++ src/Dfe.RegionalImprovementForStandardsAndExcellence/Pages/Summary.cshtml.cs
@@ -2,47 +2,54 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Dfe.RegionalImprovementForStandardsAndExcellence.Frontend.Services;
 using Dfe.RegionalImprovementForStandardsAndExcellence.Application.SupportProject.Queries;
+using Dfe.RegionalImprovementForStandardsAndExcellence.Frontend.Models;
+using MediatR;
+using Dfe.RegionalImprovementForStandardsAndExcellence.Application.SupportProject.Commands.CreateSupportProject;
 
 namespace Dfe.RegionalImprovementForStandardsAndExcellence.Frontend.Pages;
 
 public class SummaryModel : PageModel
 {
-   private readonly IGetEstablishment _getEstablishment;
-   private readonly ISupportProjectQueryService _supportProjectQueryService;
+    private readonly IGetEstablishment _getEstablishment;
+    private readonly ISupportProjectQueryService _supportProjectQueryService;
+    private readonly IMediator _mediator;
 
-   public SummaryModel(IGetEstablishment getEstablishment,
-                       ISupportProjectQueryService supportProjectQueryService
-       )
-   {
-      _getEstablishment = getEstablishment;
-      _supportProjectQueryService = supportProjectQueryService;
-   }
+    public SummaryModel(IGetEstablishment getEstablishment,
+                        ISupportProjectQueryService supportProjectQueryService, 
+                        IMediator mediator
+        )
+    {
+        _getEstablishment = getEstablishment;
+        _supportProjectQueryService = supportProjectQueryService;
+        _mediator = mediator;
+    }
 
-   public DfE.CoreLibs.Contracts.Academies.V4.Establishments.EstablishmentDto Establishment { get; set; }
-   
-   public async Task<IActionResult> OnGetAsync(string urn)
-   {
-      Establishment = await _getEstablishment.GetEstablishmentByUrn(urn);
-      
-      return Page();
-   }
+    public DfE.CoreLibs.Contracts.Academies.V4.Establishments.EstablishmentDto Establishment { get; set; }
 
-   //public async Task<IActionResult> OnPostAsync(string urn)
-   //{
-   //   DfE.CoreLibs.Contracts.Academies.V4.Establishments.EstablishmentDto establishment = await _getEstablishment.GetEstablishmentByUrn(urn);
-      
+    public async Task<IActionResult> OnGetAsync(string urn)
+    {
+        Establishment = await _getEstablishment.GetEstablishmentByUrn(urn);
 
-   //   //var result = await _supportProjectQueryService.CreateSupportProject(CreateSupportProjectMapper.MapToDto(establishment));
+        return Page();
+    }
 
-   //   //if (result.Success)
-   //   //{
-   //   //   return RedirectToPage(Links.ProjectList.Index.Page);
-   //   //}
+    public async Task<IActionResult> OnPostAsync(string urn)
+    {
+        DfE.CoreLibs.Contracts.Academies.V4.Establishments.EstablishmentDto establishment = await _getEstablishment.GetEstablishmentByUrn(urn);
 
-   //   //else
-   //   //{
-   //   //   return RedirectToPage(Links.NewProject.Summary.Page);
-   //   //}
-   //}
-   
+        var request = new CreateSupportProjectCommand(establishment.Name, establishment.Urn, establishment.Gor.Name, null);
+
+        var id = await _mediator.Send(request);
+
+        if (id != null)
+        {
+            return RedirectToPage(Links.ProjectList.Index.Page);
+        }
+
+        else
+        {
+            return RedirectToPage(Links.NewProject.Summary.Page);
+        }
+    }
+
 }
