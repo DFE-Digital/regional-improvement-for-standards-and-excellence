0053655b85d272ee41570438b03b66fb10ce415c
danielryannimble <danielryan@nimbleapproach.com>
2025-01-02 12:34:05 +0000

    support project base

--- src/Dfe.RegionalImprovementForStandardsAndExcellence/Pages/TaskList/Index.cshtml.cs
+++ src/Dfe.RegionalImprovementForStandardsAndExcellence/Pages/TaskList/Index.cshtml.cs
@@ -8,47 +8,27 @@ using Dfe.RegionalImprovementForStandardsAndExcellence.Frontend.Services;
 
 namespace Dfe.RegionalImprovementForStandardsAndExcellence.Frontend.Pages.TaskList;
 
-public class IndexModel(
-    ISupportProjectQueryService _supportProjectQueryService,
-                  ErrorService errorService,IGetEstablishment _getEstablishment
-    ) : PageModel
+public class IndexModel : BaseSupportProjectPageModel
 {
-   //protected readonly ISession _session = session;
-   public SupportProjectViewModel SupportProject { get; set; }
    public string ReturnPage { get; set; }
 
    public void SetErrorPage(string errorPage)
    {
       TempData["ErrorPage"] = errorPage;
    }
+   
+   public IndexModel(ISupportProjectQueryService supportProjectQueryService, IGetEstablishment getEstablishment) : base(supportProjectQueryService,getEstablishment)
+   {
+       
+   }
 
    public async Task<IActionResult> OnGetAsync(int id, CancellationToken cancellationToken)
     {
         ProjectListFilters.ClearFiltersFrom(TempData);
 
-        var result = await _supportProjectQueryService.GetSupportProject(id, cancellationToken);
-
-        if (result.IsSuccess && result.Value != null)
-        {
-            SupportProject = SupportProjectViewModel.Create(result.Value);
-            
-            DfE.CoreLibs.Contracts.Academies.V4.Establishments.EstablishmentDto establishment = await _getEstablishment.GetEstablishmentByUrn(result.Value.schoolUrn);
-            
-            SupportProject.QualityOfEducation = establishment.MISEstablishment.QualityOfEducation;
-            SupportProject.LastInspectionDate = establishment.OfstedLastInspection;
-            SupportProject.BehaviourAndAttitudes = establishment.MISEstablishment.BehaviourAndAttitudes;
-            SupportProject.PersonalDevelopment = establishment.MISEstablishment.PersonalDevelopment;
-            SupportProject.LeadershipAndManagement = establishment.MISEstablishment.EffectivenessOfLeadershipAndManagement;
-        }
-        
-        
-
         ReturnPage = @Links.ProjectList.Index.Page;
-
-        if (!result.IsSuccess)
-        {
-            return NotFound();
-        }
+        
+        await base.OnGetAsync1(id, cancellationToken);
 
         return Page();
    }
