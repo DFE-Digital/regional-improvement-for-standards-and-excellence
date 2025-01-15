21557394ee86a2fbbfe952a950736024cf748956
danielryannimble <danielryan@nimbleapproach.com>
2025-01-15 15:02:56 +0000

    tag helpers and place holder example

---
+++ src/Dfe.RegionalImprovementForStandardsAndExcellence/ViewModels/CheckboxInputViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Dfe.RegionalImprovementForStandardsAndExcellence.Frontend.ViewModels;
+
+public class CheckboxInputViewModel
+{
+   public string Id { get; set; }
+   public string Name { get; set; }
+   public string Value { get; set; }
+   public string Label { get; set; }
+   public string ErrorMessage { get; set; }
+}
