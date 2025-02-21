94525f3dec84ff547cf548b2a37698568b6c6568
mshakirdfe <muhammad.shakir@education.gov.uk>
2025-02-21 12:10:27 +0000

    Display error or page not found page based on error code (#140)

---
+++ src/Tests/Dfe.ManageSchoolImprovement.Frontend.Tests/Pages/Errors/IndexTests.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Dfe.ManageSchoolImprovement.Frontend.Pages.Errors;
+using Microsoft.AspNetCore.Routing;
+
+namespace Dfe.ManageSchoolImprovement.Frontend.Tests.Pages.Errors
+{
+    public class IndexTests
+    {
+        private readonly Mock<HttpContext> _httpContextMock;
+        private readonly IndexModel _model;
+
+        public IndexTests()
+        {
+            _httpContextMock = new Mock<HttpContext>();
+
+            ActionContext actionContext = new(_httpContextMock.Object, new RouteData(), new PageActionDescriptor(), new ModelStateDictionary());
+            PageContext pageContext = new(actionContext);
+            _model = new IndexModel { PageContext = pageContext };
+        }
+
+        [Theory]
+        [InlineData(404, "Page not found")]
+        [InlineData(500, "Internal server error")]
+        [InlineData(501, "Not implemented")]
+        [InlineData(99999, "Error 99999")]
+        public void OnGet_WhenResponseHasAStatusCode_SetsMessageCorrectly(int statusCode, string expectedMessage)
+        {
+            _model.OnGet(statusCode);
+
+            Assert.Equal(_model.ErrorMessage, expectedMessage);
+        }
+
+        [Fact]
+        public void OnGet_WhenUnhandledError_HasDefaultMessage()
+        {
+            Mock<IFeatureCollection> mockIFeatureCollection = new();
+            mockIFeatureCollection.Setup(collection => collection.Get<IExceptionHandlerPathFeature>())
+               .Returns(new ExceptionHandlerFeature { Error = new Exception() });
+            _httpContextMock.Setup(context => context.Features).Returns(mockIFeatureCollection.Object);
+
+            _model.OnGet();
+
+            Assert.Equal("Sorry, there is a problem with the service", _model.ErrorMessage);
+        }
+
+        [Fact]
+        public void OnGet_WhenUnhandledNoPageNamedError_SetsPageNotFoundMessageAndStatus()
+        {
+            Mock<IFeatureCollection> mockIFeatureCollection = new();
+            mockIFeatureCollection.Setup(collection => collection.Get<IExceptionHandlerPathFeature>())
+               .Returns(new ExceptionHandlerFeature { Error = new InvalidOperationException("No page named") });
+            _httpContextMock.Setup(context => context.Features).Returns(mockIFeatureCollection.Object);
+            _httpContextMock.SetupSet(context => context.Response.StatusCode = 404).Verifiable();
+
+            _model.OnGet();
+             
+            Assert.Equal("Page not found", _model.ErrorMessage);
+            _httpContextMock.Verify();
+        }
+
+        [Theory]
+        [InlineData(404, "Page not found")]
+        [InlineData(500, "Internal server error")]
+        [InlineData(501, "Not implemented")]
+        [InlineData(99999, "Error 99999")]
+        public void OnPost_WhenResponseHasAStatusCode_SetsMessageCorrectly(int statusCode, string expectedMessage)
+        {
+            _model.OnPost(statusCode);
+
+            Assert.Equal(_model.ErrorMessage, expectedMessage);
+        }
+
+        [Fact]
+        public void OnPost_WhenUnhandledError_HasDefaultMessage()
+        {
+            Mock<IFeatureCollection> mockIFeatureCollection = new();
+            mockIFeatureCollection.Setup(collection => collection.Get<IExceptionHandlerPathFeature>())
+               .Returns(new ExceptionHandlerFeature { Error = new Exception() });
+            _httpContextMock.Setup(context => context.Features).Returns(mockIFeatureCollection.Object);
+
+            _model.OnPost();
+
+            Assert.Equal("Sorry, there is a problem with the service", _model.ErrorMessage);
+        }
+
+        [Fact]
+        public void OnPost_WhenUnhandledNoPageNamedError_SetsPageNotFoundMessageAndStatus()
+        {
+            Mock<IFeatureCollection> mockIFeatureCollection = new();
+            mockIFeatureCollection.Setup(collection => collection.Get<IExceptionHandlerPathFeature>())
+               .Returns(new ExceptionHandlerFeature { Error = new InvalidOperationException("No page named") });
+            _httpContextMock.Setup(context => context.Features).Returns(mockIFeatureCollection.Object);
+            _httpContextMock.SetupSet(context => context.Response.StatusCode = 404).Verifiable();
+
+            _model.OnPost();
+
+            Assert.Equal("Page not found", _model.ErrorMessage);
+            _httpContextMock.Verify();
+        }
+    }
+}
