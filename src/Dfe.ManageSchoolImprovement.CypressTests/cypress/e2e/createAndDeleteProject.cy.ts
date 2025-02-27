4b01463afc0eba4ddb6806f4199161dabe98fd75
Dan Good <dangood84@me.com>
2025-02-27 16:59:19 +0000

    Added 'has' assertion function calls to createAndDeleteProject.cy.ts spec file

--- src/Dfe.ManageSchoolImprovement.CypressTests/cypress/e2e/createAndDeleteProject.cy.ts
+++ src/Dfe.ManageSchoolImprovement.CypressTests/cypress/e2e/createAndDeleteProject.cy.ts
@@ -8,29 +8,49 @@ import path from "path";
 
 describe("User navigates to the rise landing page", () => {
     let school = 'Coom'
+    let header = 'Which school needs help?'
+    let URN = '105443'
+    let localAuthority = 'Manchester'
+    let region = 'North West'
+    let schoolType = 'Community school'
+    let faithSchool = 'Does not apply'
+    let ofstedRating = 'Good'
+    let lastInspection = '16 January 2011'
+    let pfi = 'Does not apply'
 
     beforeEach(() => {
-        cy.login();
+        cy.login()
         cy.url().should('contains', 'schools-requiring-improvement')   
     });
 
-    it("Should be able to see Add a school option and school list", () => {
+    it("Should be able to add a school and add it to the list", () => {
        riseHomePage
          .AddSchool()
 
        cy.executeAccessibilityTests()
 
-       whichSchoolNeedsHelp.withSchoolName(school)
-
-       whichSchoolNeedsHelp.clickContinue()
+       whichSchoolNeedsHelp.hasHeader(header)
+                           .withSchoolName(school)
+                           .clickContinue()
 
        cy.executeAccessibilityTests()
 
+       checkSchoolDetails.hasHeader(header)
+                         .hasSchoolName(school)
+                         .hasURN(URN)
+                         .hasLocalAuthority(localAuthority)
+                         .hasSchoolType(schoolType)
+                         .hasFaithSchool(faithSchool)
+                         .hasOfstedRating(ofstedRating)
+                         .hasLastInspection(lastInspection)
+                         .hasPFI(pfi)
+
        checkSchoolDetails.clickContinue()
 
-       riseHomePage.selectSchool(school)
-
-      // aboutTheSchool.deleteSchool()
+       riseHomePage.hasSchoolName(school)
+                   .hasURN(URN)
+                   .hasLocalAuthority(localAuthority)
+                   .hasRegion(region)
 
     });
 });
